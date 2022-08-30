72973be23ae425395fedc544a3b42b2487d47a03
felix <perezfelixernesto@gmail.com>
2022-08-30 16:52:55 -0500

    master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handleSubtract = () => {
+    setCount(count - 1);
+  };
+  const handleIncrease = () => {
+    setCount(count + 1);
+  };
+  const handleReset = () => {
+    setCount(0);
+  };
+
+  let color = '#fff';
+  if (count >= 1) {
+    color = 'green'
+  } else if (count < 0) {
+    color = 'red'
+  } else 
+    color = 'white'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          felix
-        </a>
-      </header>
+      <div className="wrapper">
+        <div className="hdr">
+          <h1>React Counter App</h1>
+        </div>
+        <div className="count">
+          <span className="num" style = {{color: color}}>{count}</span>
+        </div>
+        <div className="btns">
+          <button className="decrease" onClick={handleSubtract}>decrease</button>
+          <button className="reset" onClick={handleReset}>reset</button>
+          <button className="increase" onClick={handleIncrease}>increase</button>
+        </div>
+      </div>
     </div>
   );
 }

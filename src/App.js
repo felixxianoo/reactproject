ff00d07616803c01c0d545c0e17b6ca0894d9059
felix <perezfelixernesto@gmail.com>
2022-09-23 10:31:17 -0500

    change2timer

--- src/App.js
+++ src/App.js
@@ -1,39 +1,49 @@
 import styles from './App.css';
 import React, {useState} from 'react';
+import { useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const handleSubtract = () => {
-    setCount(count - 1);
-  };
-  const handleIncrease = () => {
-    setCount(count + 1);
-  };
-  const handleReset = () => {
-    setCount(0);
-  };
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
 
-  let color = '#fff';
-  if (count >= 1) {
-    color = 'green'
-  } else if (count < 0) {
-    color = 'red'
-  } else 
-    color = 'white'
+  let timer;
+  useEffect(() => {
+  timer = setInterval(() => {
+      setSeconds(seconds + 1);
+      if (seconds === 59) {
+        setMinutes(minutes + 1);
+        setSeconds(0);
+      }
+    }, 1000);
+    return () => clearInterval(timer)
+  })
+
+  const pause = () => {
+    clearInterval(timer);
+  }
+
+  const reset = () => {
+    setMinutes(0);
+    setSeconds(0);
+  }
+
 
   return (
     <div className="App">
       <div className="wrapper">
         <div className="hdr">
-          <h1>React Counter App</h1>
+          <h1>React Timer App</h1>
         </div>
         <div className="count">
-          <span className="num" style = {{color: color}}>{count}</span>
+          <span className='num'>
+            {minutes < 10 ? '0' + minutes : minutes}:
+            {seconds < 10 ? '0' + seconds : seconds}
+            </span>
         </div>
         <div className="btns">
-          <button className="decrease" onClick={handleSubtract}>decrease</button>
-          <button className="reset" onClick={handleReset}>reset</button>
-          <button className="increase" onClick={handleIncrease}>increase</button>
+          <button onClick={pause}>pause</button>
+          <button onClick={reset}>reset</button>
+          <button>start</button>
         </div>
       </div>
     </div>
